1b5447eb78d19fe011c223c7c5fe39a883983353
minh duc <minhduc261298@gmail.com>
2022-10-12 02:30:45 +0700

    add url

--- src/shared/constants/env.ts
+++ src/shared/constants/env.ts
@@ -4,6 +4,6 @@ export const isClient = !isServer;
 
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = NODE_ENV === 'development' ? (process.env.PORT || 3000) : '';
 
 export const BASE_URL = process.env.BASE_URL || 'http://localhost';
