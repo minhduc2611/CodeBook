7269b6beb304b7c49f5e2d7138d7213cdcb0183e
minh duc <minhduc261298@gmail.com>
2022-10-12 09:58:16 +0700

    modify url

--- src/client/graphql/client/index.ts
+++ src/client/graphql/client/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { BASE_URL, PORT } from './../../../shared/constants/env';
+import { BASE_URL, NODE_ENV, PORT } from './../../../shared/constants/env';
 
 export const AppApolloClient = new ApolloClient({
-  uri: `${BASE_URL}:${PORT}/graphql`,
+  uri: NODE_ENV === 'development' ? `${BASE_URL}:${PORT}/graphql` : `${BASE_URL}/graphql`,
   cache: new InMemoryCache()
 });
