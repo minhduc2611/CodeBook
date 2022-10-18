67dd56cbdab9ff2ad1fbd73883bf216e55c6afde
minh duc <minhduc261298@gmail.com>
2022-10-18 22:10:50 +0700

    add thing url

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,30 +1,92 @@
-import { GetServerSideProps } from 'next';
-import Link from 'next/link';
+import * as esbuild from 'https://cdn.jsdelivr.net/npm/@netlify/esbuild-wasm@0.13.6/esm/browser.min.js';
 import { FC } from 'react';
 import { BlogPost } from './../../shared/blog-post';
-import { customFetch } from './../../shared/utils/fetch';
 
 type THomeProps = {
   blogPosts?: BlogPost[];
 };
 
-const Home: FC<THomeProps> = ({ blogPosts = [] }) => {
+/**
+ * ==========
+ */
+
+import { useEffect } from 'react';
+async function compile(code) {
+  await esbuild.initialize({
+    wasmURL:
+      'https://cdn.jsdelivr.net/npm/@netlify/esbuild-wasm@0.13.6/esbuild.wasm'
+  });
+
+  const options = {
+    loader: 'jsx'
+  };
+
+  try {
+    const result = await esbuild.transform(code, options);
+    return result.code;
+  } catch (err) {
+    console.log(err);
+  }
+}
+const code = `
+  import React from 'https://cdn.skypack.dev/react';
+  import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+  import a from './sample';
+  console.log(a);
+
+  function App() {
+    const [count, setCount] = React.useState(0);
+
+    const clicked = () => {
+      setCount(count + 1)
+    }
+
+    return (
+      <div className='container'>
+        <div>Testing</div>
+        <button onClick={clicked}>Clicked me times {count}</button>
+      </div>
+    );
+  }
+
+  ReactDOM.render(<App />, document.body);
+  `;
+
+function App11() {
+  useEffect(() => {
+    compile(code).then((compiled) => {
+      console.log('compiled', compiled);
+    });
+  }, []);
+  return (
+    <div className="child-component">
+      <p>My context:</p>
+    </div>
+  );
+}
+
+/**
+ * ==========
+ */
+const Home: FC<THomeProps> = ({}) => {
   return (
     <div>
       <h1>Home</h1>
-      {blogPosts.map(({ title, id }) => (
+      App11
+      {/* {blogPosts.map(({ title, id }) => (
         <div key={id}>
           <Link href={`/posts/${id}`}>{title}</Link>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<THomeProps> = async () => {
-  const blogPosts = await customFetch<BlogPost[]>('/api/blog-posts');
+// export const getServerSideProps: GetServerSideProps<THomeProps> = async () => {
+//   const blogPosts = await customFetch<BlogPost[]>('/api/blog-posts');
 
-  return { props: { blogPosts } };
-};
+//   return {  };
+// };
 
 export default Home;
--- src/pages/types.d.ts
+++ src/pages/types.d.ts
@@ -1,2 +1,3 @@
 // Declare some module that has value in it, but doesn't have types
 declare module 'monaco-jsx-highlighter';
+declare module 'https://cdn.jsdelivr.net/npm/@netlify/esbuild-wasm@0.13.6/esm/browser.min.js';
