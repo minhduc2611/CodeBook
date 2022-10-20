ac5f5f32e41c56c41ae0c34284f49c5b68d27435
minh duc <minhduc261298@gmail.com>
2022-10-20 18:27:40 +0700

    add vercel

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { RenderModule } from 'nest-next';
-import Next from 'next';
+// import Next from 'next';
 import { AppModule } from './server/app.module';
 import { NODE_ENV, PORT } from './shared/constants/env';
 
 async function bootstrap() {
   const dev = NODE_ENV !== 'production';
-  const client = Next({ dev });
-  await client.prepare();
+  // const client = Next({ dev });
+  // await client.prepare();
   const server = await NestFactory.create(AppModule);
   const renderer = server.get(RenderModule);
   /* viewsDir null means that nest-next should look for pages in root dir */
-  renderer.register(server, client, { viewsDir: null, passthrough404: true });
+  // renderer.register(server, client, { viewsDir: null, passthrough404: true });
+  
+  
+  
+  
+  
   await server.listen(PORT);
 }
 bootstrap();
